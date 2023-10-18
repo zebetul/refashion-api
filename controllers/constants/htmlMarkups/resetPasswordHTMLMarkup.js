2d448c830837a7e080ac1e5ee311a02b53ab9f9b
zebetul <sebenicristi@gmail.com>
2023-10-18 23:25:22 +0300

    Reset password functionality implementation. Step 2, creating token and sending email to user with reset link.

---
+++ controllers/constants/htmlMarkups/resetPasswordHTMLMarkup.js
@@ -0,0 +1,87 @@
+const resetPasswordHTMLMarkup = (token) => {
+  return `
+    <div style="text-align: center">
+      <div style="display: inline-block; vertical-align: middle">
+        <a href="https://restil.ro" target="_blank">
+          <img
+            src="https://rfs-logo-images.s3.eu-west-1.amazonaws.com/restil-h_50_px.webp"
+            alt="Restil Logo"
+            style="height: 50px; margin: 20px 0"
+          />
+        </a>
+
+        <h1 style="margin: 20px 0">Salut,</h1>
+
+        <div style="margin: 50px 0">
+          <p style="margin: 0 0 20px 0">
+            Click pe butonul de mai jos pentru a reseta parola contului tău.
+          </p>
+
+          <a
+            href="https://restil.ro/resetare-parola/${token}"
+            style="
+              text-decoration: none;
+              color: #fff;
+              background-color: #3cb371;
+              padding: 10px 20px;
+              border-radius: 20px;
+            "
+            target="_blank"
+          >
+            Resetează parola
+          </a>
+        </div>
+
+        <p style="margin: 20px 0">
+          Dacă nu ai făcut tu această cerere, ignoră acest email.
+        </p>
+
+        <p style="margin: 20px 0">
+          P.S. Dacă ai întrebări, ne poți contacta la
+          <a
+            href="mailto: contact@restil.ro"
+            style="text-decoration: none; color: #3cb371"
+          >
+            contact@restil
+          </a>
+          .
+        </p>
+
+        <p style="margin: 20px 0">Echipa Restil</p>
+      </div>
+
+      <footer
+        style="
+          height: auto;
+          margin: 20px auto;
+          column-gap: 0.5rem;
+          text-align: center;
+          display: table;
+          width: auto;
+        "
+      >
+        <span
+          style="
+            display: inline-block;
+            line-height: 50px;
+            padding-right: 0.5rem;
+          "
+        >
+          <img
+            src="https://rfs-logo-images.s3.eu-west-1.amazonaws.com/restil-h_50_px.webp"
+            alt="Restil Logo"
+            style="height: 25px"
+          />
+        </span>
+
+        <span style="display: inline-block; line-height: 50px">© 2023</span>
+
+        <span style="display: inline-block; line-height: 50px">
+          Toate drepturile rezervate.
+        </span>
+      </footer>
+    </div>
+  `;
+};
+
+export default resetPasswordHTMLMarkup;
--- controllers/handleResetPassword.js
+++ controllers/handleResetPassword.js
@@ -1,32 +1,39 @@
 import { sendEmailTo } from "./helpers.js";
+import crypto from "crypto";
+import resetPasswordHTMLMarkup from "./constants/htmlMarkups/resetPasswordHTMLMarkup.js";
 
 const handleResetPassword = async (req, res, db) => {
   const email = req.body.email;
-
-  console.log(email);
 
   if (!email) {
     return res.json("Missing email");
   }
 
-  const user = await db("users").select("*").where("email", "=", email).first();
-
-  console.log(user);
+  const user = await db("login").select("*").where("email", "=", email).first();
 
   if (!user) {
     return res.json("Email not found");
   }
 
-  // Send email with reset password link
-  //   const response = await sendEmailTo(
-  //     email,
-  //     "Reset password",
-  //     `Click here to reset your password: https://restil.ro/reset_password/${user.id}`
-  //   );
+  // Generate token
+  const token = await db("tokens")
+    .insert({
+      user_id: user.userid,
+      token: crypto.randomBytes(16).toString("hex"),
+    })
+    .returning("token")
+    .then((token) => token[0]);
 
-  //   if (response.error) {
-  //     return res.status(400).json({ error: response.error });
-  //   }
+  // Send email with reset password link
+  const response = await sendEmailTo(
+    email,
+    "Reset password",
+    resetPasswordHTMLMarkup(token)
+  );
+
+  if (response.error) {
+    return res.status(400).json({ error: response.error });
+  }
 };
 
 export default handleResetPassword;
