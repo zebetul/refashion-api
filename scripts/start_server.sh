145065c6054b4a1df6d19e743f84f063c4d8aa87
zebetul <sebenicristi@gmail.com>
2023-12-15 17:36:31 +0200

    HOME env variable set in the stop_server.sh and start_server.sh scripts.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Set HOME environment variable
+export HOME=/home/ec2-user
+
 # Start the Node.js server using pm2
 cd /home/ec2-user/configs
 /usr/local/bin/pm2 start ecosystem.config.js
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Set HOME environment variable
+export HOME=/home/ec2-user
+
 # Stop pm2 process for the app
 cd /home/ec2-user/refashion-api/
 /usr/local/bin/pm2 stop refashion-api
